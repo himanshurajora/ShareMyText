22364439ff0245bee33ecb49a3fe8f328161bac2
Himanshu Jangid <iamahalofan@gmail.com>
2021-10-16 20:44:00 +0530

    decryption and encryption

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 import firebase from 'firebase'
 import app from 'firebase/app'
 import 'firebase/firestore'
-
+import * as Crypto from 'crypto-js'
 
 var firebaseConfig = {
   apiKey: "AIzaSyCTvOjkdho2r7l4m-GVPYiSrEuazQeYu2s",
@@ -16,43 +16,73 @@ var firebaseConfig = {
   measurementId: "G-WWFW3PM397"
 }
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig) 
+
+interface Data{
+  data: string,
+  createdAt: number
+}
 
 function App() {
-
+  
   var db = firebase.firestore()
   var ref = db.collection("shareData")
   const [data, setData] = useState("")
   const [message, setMessage] = useState("")
-  const [disable, setDisable] = useState(false) 
+  const [disable, setDisable] = useState(false)
+  const [encypcode, setencypcode] = useState("")
+  const [decryptcode, setdecryptcode] = useState("")
+  const [decoded, setdecoded] = useState("")
   const [r, setR] = useState("")
   const shareData = async () => {
     setDisable(true)
     if (data) {
-      var dataJson = {
-        "data": data,
-        "createdAt": Date.now()
+      var dataJson = {"data" : "some data", "createdAt": 1337}
+      if (encypcode) {
+       dataJson = {
+          "data": Crypto.AES.encrypt(data, encypcode).toString(),
+          "createdAt": Date.now(),
+        }
+      } else {
+        dataJson = {
+          "data": data,
+          "createdAt": Date.now(),
+        }
       }
 
-      await ref.doc("shared").set(dataJson)
-      setMessage(`shared data successfully at ${new Date(dataJson.createdAt).toUTCString()}`)
+        await ref.doc("shared").set(dataJson)
+        setMessage(`shared data successfully at ${new Date(dataJson.createdAt).toUTCString()}`)
+      }
+      setDisable(false)
     }
-    setDisable(false)
+
+    const decryptData = async () => {
+     try{
+      if (r) {
+        var decoded  = Crypto.AES.decrypt(r, decryptcode).toString(Crypto.enc.Utf8)
+        setdecoded(decoded)
+        // setMessage(`decrypted data successfully at ${new Date(decoded.createdAt).toUTCString()}`)
+      }
+      } catch(e) {
+          setdecoded("wrong decreption key or some error".toUpperCase())
+      }
+     }
+
+
+    ref.doc("shared").onSnapshot((snapshot) => {
+      setR(snapshot.data()!.data)
+    })
+
+    return (
+      <div className="App">
+        <textarea rows={20} onChange={(e) => { setData(e.target.value) }} placeholder={"Enter You Text Here"}></textarea>
+        <p> <input type="text" placeholder='Encryption Code' onChange={(e)=>{setencypcode(e.target.value)}} /> <span> <button onClick={shareData}>{!disable ? "Share" : "Sending In Progress..."}</button></span> {message}</p>
+        <p>Recieved data here:</p>
+        <pre id={"r"}>{r}</pre>
+        <pre id={"decoded"}>{decoded}</pre>
+        <p> <input type="text" placeholder='Decryption Code' onChange={(e)=>{setdecryptcode(e.target.value)}} /> <span> <button onClick={decryptData}>{"Decrypt Data"}</button></span></p>
+      </div>
+    )
   }
 
-  ref.doc("shared").onSnapshot((snapshot)=>{
-    console.log(snapshot.data())
-    setR(snapshot.data()!.data)
-  })
-
-  return (
-    <div className="App">
-      <textarea rows={20} onChange={(e) => { setData(e.target.value) }} placeholder={"Enter You Text Here"}></textarea>
-      <p> <span> <button onClick={shareData}>{!disable ? "Share" : "Sending In Progress..."}</button></span> {message}</p>
-      <p>Recieved data here:</p>
-      <p id={"r"}>{r}</p>
-    </div>
-  )
-}
-
-export default App
+  export default App
